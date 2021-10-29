c7747b628f03c5da8579a95380702405b804058f
enzo-1502 <RM87661@fiap.com.br>
2021-10-28 21:57:21 -0300

    criada a interação para as listas dos filmes com javascript

--- src/components/movierow/MovieRow.js
+++ src/components/movierow/MovieRow.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+import {MdNavigateBefore, MdNavigateNext} from "react-icons/md";
 
 const StyledMovieRow = styled.div `
     .movieRow {
@@ -16,7 +17,8 @@ const StyledMovieRow = styled.div `
     }
 
     .movieRow--list {
-        width: 99999999px;
+        transition: all ease 0.5s;
+    
     }
     .movieRow--item {
         display: inline-block;
@@ -33,15 +35,75 @@ const StyledMovieRow = styled.div `
     .movieRow--item img:hover {
         transform: scale(1);
     }
+
+    .movieRow--left, 
+    .movieRow--right {
+        position: absolute;
+        width: 40px;
+        height: 300px;
+        z-index: 99;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+        background-color: rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+        opacity: 0;
+        transition: all ease 0.5s;
+
+    }
+
+    .movieRow--left {
+        left: 0;
+    }
+
+    .movieRow--right {
+        right: 0;
+    }
+
+    .movieRow:hover .movieRow--left,
+    .movieRow:hover .movieRow--right {
+        opacity: 1;
+    }
 `
 
 export default ({title, items}) => {
+
+    const [scrollX, setScrollX] = useState(-400)
+    // função para passar a lista para a esquerda
+    const handleLeftArrow = ()=> {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+    // função para passar a lista para a direita 
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 200;
+        if((window.innerWidth - listW > x)) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
         <StyledMovieRow>
             <div className="movieRow">
                 <h2>{title}</h2>
+                <div className="movieRow--left" onClick={handleLeftArrow}>
+                    <MdNavigateBefore style={{fontSize: 70}}/>
+                </div>
+                <div className="movieRow--right" onClick={handleRightArrow}>
+                    <MdNavigateNext style={{fontSize: 70}}/>
+                </div>
+
                 <div className="movieRow--listarea">
-                    <div className="movieRow--list">
+                    <div className="movieRow--list" style={{
+                        marginLeft: scrollX,
+                        width: items.results.length * 200
+                    }}>
                         {items.results.length > 0 && items.results.map((item, key)=>(
                             <div className="movieRow--item">
                                 <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
